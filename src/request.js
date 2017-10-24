70e1df6f973b45ad5ca75fcd75cd9890e584d565
Dmitriy Rozhkov <drozhkov@mesosphere.io>
2017-10-24 18:06:50 +0200

    feat(http-service): release http-service

---
+++ src/request.js
@@ -0,0 +1,29 @@
+import Rx from "rxjs";
+
+import ConnectionManager from "@dcos/connection-manager";
+import { XHRConnection, ConnectionEvent } from "@dcos/connections";
+
+export default function request(url, options = {}) {
+  return Rx.Observable.create(function(observer) {
+    const connection = new XHRConnection(url, options);
+
+    connection.addListener(ConnectionEvent.ERROR, function(event) {
+      observer.error({
+        code: event.target.xhr.status,
+        message: event.target.xhr.statusText
+      });
+    });
+
+    connection.addListener(ConnectionEvent.COMPLETE, function(event) {
+      observer.next(event.target.response);
+      observer.complete();
+    });
+
+    ConnectionManager.enqueue(connection);
+
+    // Closing the connection when there are no subscribers left
+    return function teardown() {
+      ConnectionManager.dequeue(connection);
+    };
+  });
+}
---
+++ src/stream.js
@@ -0,0 +1,30 @@
+import Rx from "rxjs";
+
+import ConnectionManager from "@dcos/connection-manager";
+import { XHRConnection, ConnectionEvent } from "@dcos/connections";
+
+export default function stream(url, options = {}) {
+  return Rx.Observable.create(function(observer) {
+    const connection = new XHRConnection(url, options);
+
+    connection.addListener(ConnectionEvent.DATA, function(event) {
+      observer.next(event.target.response);
+    });
+    connection.addListener(ConnectionEvent.ERROR, function(event) {
+      observer.error({
+        code: event.target.xhr.status,
+        message: event.target.xhr.statusText
+      });
+    });
+    connection.addListener(ConnectionEvent.COMPLETE, function() {
+      observer.complete();
+    });
+
+    ConnectionManager.enqueue(connection);
+
+    // Closing the connection when there are no subscribers left
+    return function teardown() {
+      ConnectionManager.dequeue(connection);
+    };
+  });
+}
